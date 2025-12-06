6c0f66dd457a87ba27626d35b8dea3e4ea46d001
Francis Duvivier <francisduvivier@gmail.com>
2025-12-06 23:29:25 +0100

    clean bad solutions

--- solutions/2015/day01/solution_part2.py
+++
@@ -1,9 +0,0 @@
-with open('input.txt') as f:
-    instructions = f.read().strip()
-
-floor = 0
-for i, c in enumerate(instructions, 1):
-    floor += 1 if c == '(' else -1
-    if floor == -1:
-        print(i)
-        break
