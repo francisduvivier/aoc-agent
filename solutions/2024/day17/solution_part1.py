10ba9f242971bb984f07e26181cf02e1dba9dc05
Agentic Francis Duvivier <francisduvivier+agent@gmail.com>
2025-12-07 17:59:30 +0000

    Agent generated solution_part1 for 2024 day 17 (attempt 6)

--- solutions/2024/day17/solution_part1.py
+++ solutions/2024/day17/solution_part1.py
@@ -5,7 +5,7 @@ def solve_part1(lines):
     C = int(lines[2].split(': ')[1])
     
     # Parse program
-    program = [int(x) for x in lines[4].split(': ')[1].split(',')]
+    program = list(map(int, lines[4].split(': ')[1].split(',')))
     
     # Run program
     ip = 0
@@ -16,47 +16,80 @@ def solve_part1(lines):
         operand = program[ip + 1]
         
         if opcode == 0:  # adv
-            denom = 1 << combo_value(operand, A, B, C)
+            if operand <= 3:
+                denom = 1 << operand
+            elif operand == 4:
+                denom = 1 << A
+            elif operand == 5:
+                denom = 1 << B
+            elif operand == 6:
+                denom = 1 << C
             A = A // denom
             ip += 2
+            
         elif opcode == 1:  # bxl
             B = B ^ operand
             ip += 2
+            
         elif opcode == 2:  # bst
-            B = combo_value(operand, A, B, C) % 8
+            if operand <= 3:
+                val = operand
+            elif operand == 4:
+                val = A
+            elif operand == 5:
+                val = B
+            elif operand == 6:
+                val = C
+            B = val % 8
             ip += 2
+            
         elif opcode == 3:  # jnz
             if A != 0:
                 ip = operand
             else:
                 ip += 2
+                
         elif opcode == 4:  # bxc
             B = B ^ C
             ip += 2
+            
         elif opcode == 5:  # out
-            output.append(combo_value(operand, A, B, C) % 8)
+            if operand <= 3:
+                val = operand
+            elif operand == 4:
+                val = A
+            elif operand == 5:
+                val = B
+            elif operand == 6:
+                val = C
+            output.append(str(val % 8))
             ip += 2
+            
         elif opcode == 6:  # bdv
-            denom = 1 << combo_value(operand, A, B, C)
-            A = A // denom
+            if operand <= 3:
+                denom = 1 << operand
+            elif operand == 4:
+                denom = 1 << A
+            elif operand == 5:
+                denom = 1 << B
+            elif operand == 6:
+                denom = 1 << C
+            B = A // denom
             ip += 2
+            
         elif opcode == 7:  # cdv
-            denom = 1 << combo_value(operand, A, B, C)
-            A = A // denom
+            if operand <= 3:
+                denom = 1 << operand
+            elif operand == 4:
+                denom = 1 << A
+            elif operand == 5:
+                denom = 1 << B
+            elif operand == 6:
+                denom = 1 << C
+            C = A // denom
             ip += 2
     
-    return ','.join(str(x) for x in output)
-
-def combo_value(operand, A, B, C):
-    if operand <= 3:
-        return operand
-    elif operand == 4:
-        return A
-    elif operand == 5:
-        return B
-    elif operand == 6:
-        return C
-    return 0
+    return ','.join(output)
 
 # Sample data – may contain multiple samples from the problem statement.
 # Populate this list with (sample_input, expected_result) tuples.
@@ -66,7 +99,7 @@ Register B: 0
 Register C: 0
 
 Program: 0,1,5,4,3,0""", "4,6,3,5,6,3,5,2,1,0")
-]
+]  # TODO: fill with actual samples and expected results
 
 for idx, (sample_input, expected_result) in enumerate(samples, start=1):
     sample_result = solve_part1(sample_input.strip().splitlines())
