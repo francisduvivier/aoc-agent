2825127073b58c369e3166ae9809066d473e5a68
Agentic Francis Duvivier <francisduvivier+agent@gmail.com>
2025-12-07 16:18:02 +0000

    Agent generated solution_part1 for 2024 day 15 (attempt 10)

--- solutions/2024/day15/solution_part1.py
+++ solutions/2024/day15/solution_part1.py
@@ -1,7 +1,7 @@
 # Edit this file: implement solve_part1
 
 def solve_part1(lines):
-    # Parse the grid and moves
+    # Parse the input: split into grid and moves
     grid_lines = []
     moves = []
     parsing_moves = False
@@ -15,65 +15,80 @@ def solve_part1(lines):
         else:
             grid_lines.append(line.strip())
     
-    # Find robot position and create grid
-    robot_pos = None
-    grid = []
-    for r, row in enumerate(grid_lines):
-        grid_row = []
+    # Build the grid as a list of lists for mutability
+    grid = [list(row) for row in grid_lines]
+    
+    # Find the robot position
+    robot_r, robot_c = None, None
+    for r, row in enumerate(grid):
         for c, ch in enumerate(row):
             if ch == '@':
-                robot_pos = (r, c)
-                grid_row.append('.')
-            else:
-                grid_row.append(ch)
-        grid.append(grid_row)
+                robot_r, robot_c = r, c
+                break
+        if robot_r is not None:
+            break
     
-    # Process moves
-    r, c = robot_pos
+    # Define movement deltas
+    delta = {'^': (-1, 0), 'v': (1, 0), '<': (0, -1), '>': (0, 1)}
+    
+    # Process each move
     for move in moves:
-        dr, dc = {'^': (-1, 0), 'v': (1, 0), '<': (0, -1), '>': (0, 1)}[move]
-        nr, nc = r + dr, c + dc
+        dr, dc = delta[move]
+        nr, nc = robot_r + dr, robot_c + dc
         
-        # Check what's in the way
+        # If the next cell is a wall, nothing moves
         if grid[nr][nc] == '#':
-            # Wall - don't move
             continue
-        elif grid[nr][nc] == '.':
-            # Empty - move robot
-            r, c = nr, nc
-        else:  # grid[nr][nc] == 'O'
-            # Box - try to push it
-            # Find how far the box chain extends
-            push_chain = []
-            pr, pc = nr, nc
-            while grid[pr][pc] == 'O':
-                push_chain.append((pr, pc))
-                pr, pc = pr + dr, pc + dc
-            
-            # Check if we can push the chain
-            if grid[pr][pc] == '#':
-                # Can't push - don't move
-                continue
-            else:
-                # Can push - move the chain and robot
-                for pr, pc in reversed(push_chain):
-                    grid[pr + dr][pc + dc] = 'O'
-                    grid[pr][pc] = '.'
-                r, c = nr, nc
+        
+        # If the next cell is empty, just move the robot
+        if grid[nr][nc] == '.':
+            grid[robot_r][robot_c] = '.'
+            grid[nr][nc] = '@'
+            robot_r, robot_c = nr, nc
+            continue
+        
+        # If the next cell is a box, attempt to push it
+        if grid[nr][nc] == 'O':
+            # Find the farthest box in the line of push
+            push_path = []
+            pr, pc = robot_r, robot_c
+            while True:
+                pr += dr
+                pc += dc
+                if grid[pr][pc] == '#':
+                    # Blocked by wall, nothing moves
+                    break
+                push_path.append((pr, pc))
+                if grid[pr][pc] == '.':
+                    # Found empty space, can push
+                    # Move boxes backward along the path
+                    for i in range(len(push_path) - 1, 0, -1):
+                        r1, c1 = push_path[i]
+                        r2, c2 = push_path[i - 1]
+                        grid[r1][c1] = grid[r2][c2]
+                    # Move the robot
+                    grid[robot_r][robot_c] = '.'
+                    grid[robot_r + dr][robot_c + dc] = '@'
+                    robot_r, robot_c = robot_r + dr, robot_c + dc
+                    break
+                if grid[pr][pc] == '#':
+                    # Blocked, break without moving
+                    break
     
-    # Calculate GPS coordinates
+    # Calculate GPS coordinates sum
     total = 0
-    for r_idx, row in enumerate(grid):
-        for c_idx, ch in enumerate(row):
+    for r, row in enumerate(grid):
+        for c, ch in enumerate(row):
             if ch == 'O':
-                total += 100 * r_idx + c_idx
+                total += 100 * r + c
     
     return total
 
 # Sample data – may contain multiple samples from the problem statement.
 # Populate this list with (sample_input, expected_result) tuples.
 samples = [
-    ("""########
+    (
+        """########
 #..O.O.#
 ##@.O..#
 #...O..#
@@ -82,8 +97,11 @@ samples = [
 #......#
 ########
 
-<^^>>>vv<v>v<v<""", 2028),
-    ("""##########
+<^^>>>vv<v>>v<<""",
+        2028
+    ),
+    (
+        """##########
 #..O..O.O#
 #......O.#
 #.OO..O.O#
@@ -94,10 +112,9 @@ samples = [
 #....O...#
 ##########
 
-<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^<<>><>>v<vvv<>^v^>^<<<><<><v^<^^v><<^v<<>>v<v<v^^>vv>vvv<><>vv<<<^^>>>>v<>^>^v<v^vv^v^<^>vvv>v>^^v^v<>vv>><^<>>v>vvv^>vv<<v>^^>>^^^>><><v^<^^v>vv<^^v^<v^v^v<<<^^v<v>^<
-
-vvv<<^>^v^^<<>>>><^<<><^vv^^<>>vvv>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
-><>vv>v^v^<>>>><>><>^<<^^>vv>v<<^^^>>v^v^<^^>v^^>v^<^v>vv>vvv><^^v>^>vv<<^>v^^>vv<^^v^<v<^v>vv>>><^<>>v>vvv^>vv<<v>^^>>^^^>><><v^<^^v>vv<^^v^<v^v^v<<<^^v<v>^<""", 10092)
+<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^<<>><>>v<vvv<>^v^>^<<<><<<>v<vvv><>^<v<>^v<v^vv^v<^>vvv>v>^^v^v<>vv>><^<>>v>vvv^>vv<<v>^^>>^^^>><><v^<^^v>vv<^^v^<v^v^v<<<^^v<v>^<""",
+        10092
+    )
 ]  # TODO: fill with actual samples and expected results
 
 for idx, (sample_input, expected_result) in enumerate(samples, start=1):
